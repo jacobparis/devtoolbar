accb11f3db07a2ff47a163179f3573b256855b2e
JacobParis <jacob@jacobpariseau.com>
2021-09-01 00:02:53 +0300

    Add list of hosts

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,10 +2,43 @@ import './style.css'
 
 import * as React from 'react'
 
+const config = {
+  hosts: {
+    production: ['https://www.threeq.app'],
+    staging: ['https://blag.vercel.com'],
+    development: ['http://localhost:3000'],
+  },
+}
+
 export function DevToolbar() {
   return (
     <div className="toolbar">
       <span> Developer Toolbar </span>
+      <div>
+        <span> Host: </span>
+        <div>
+          <p> Production </p>
+          <ul>
+            {config.hosts.production.map((host) => (
+              <li key={host}> {host} </li>
+            ))}
+          </ul>
+
+          <p> Staging </p>
+          <ul>
+            {config.hosts.staging.map((host) => (
+              <li key={host}> {host} </li>
+            ))}
+          </ul>
+
+          <p> Development </p>
+          <ul>
+            {config.hosts.development.map((host) => (
+              <li key={host}> {host} </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
