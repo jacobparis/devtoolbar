6e161e964eb769e1b24b7beab04d75145b5b71f9
JacobParis <jacob@jacobpariseau.com>
2021-08-31 23:31:58 +0300

    Update toolbar

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -3,5 +3,9 @@ import './style.css'
 import * as React from 'react'
 
 export function DevToolbar() {
-  return <div className="toolbar">Developer Toolbar</div>
+  return (
+    <div className="toolbar">
+      <span> Developer Toolbar </span>
+    </div>
+  )
 }
