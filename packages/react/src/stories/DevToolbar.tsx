6a7bf4a1fb77ae34c0a9f6d59d1e0b8e1fc9d258
JacobParis <jacob@jacobpariseau.com>
2021-09-08 00:57:25 +0200

    Use hostname as toolbar title

--- packages/react/src/stories/DevToolbar.tsx
+++ packages/react/src/stories/DevToolbar.tsx
@@ -128,18 +128,32 @@ function Toolbar({hosts}: Props) {
       <div
         className={tw`font-sans px-4 py-2 bg-gray-900 text-gray-100 flex justify-between items-center`}
       >
-        <div>Developer Toolbar</div>
         {hosts && isConnected ? (
           <div>
             <button
-              className={tw`px-4 py-2`}
+              className={tw`px-4 py-2 hover:bg-gray-700 rounded-md`}
               onClick={() => setIsOpen((open) => !open)}
             >
-              Hosts
+              <span>{window.location.origin}</span>
+              <svg
+                style={{display: 'inline'}}
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
             </button>
 
             <div
-              className={tw`bg-white text-gray-800 mt-4 px-4 py-3 mx-auto border-1 rounded-md shadow-sm absolute right-0 transform transition-all ${
+              className={tw`bg-white text-gray-800 mt-4 px-4 py-3 mx-auto border-1 rounded-md shadow-sm absolute left-0 transform transition-all ${
                 isOpen ? '-translate-y-1.5' : 'opacity-0 invisible'
               }`}
               role="menu"
